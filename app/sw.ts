b4d69c01aba2505ecbad0daabb67a8fdf64b11a6
SavelevMatthew <savelevmatthew@gmail.com>
2024-05-31 13:31:37 +0500

    feat: some opts

--- app/sw.ts
+++ app/sw.ts
@@ -34,5 +34,5 @@ const serwist = new Serwist({
 serwist.addEventListeners()
 
 self.addEventListener('push', (event) => {
-  console.log(event.data)
+  console.log(JSON.stringify(event.data, null, 2))
 })
